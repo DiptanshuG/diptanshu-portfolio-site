68b68dccfe23e4993705701fa0a69c082111bc64
Diptanshu bhawsar <Diptanshubhawsar50@gmail.com>
2024-05-17 18:59:44 +0530

    temp remove hambuger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,9 +11,9 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ theme, toggleTheme }) => {
   return (
     <div className="fixed z-20 w-screen top-0 left-0 right-0 p-6 bg-transparent backdrop-filter backdrop-blur-md flex justify-between items-center">
-      <div className="cursor-pointer">
+      {/* <div className="cursor-pointer">
         <IoMenu size={60} />
-      </div>
+      </div> */}
       <button onClick={toggleTheme}>
         {theme === "light" ? <FaMoon size={40} /> : <FaSun size={40} />}
       </button>
