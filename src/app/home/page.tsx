ce2a08be48ce58e3dc22eba923f84fb6ebeb45c7
Diptanshu bhawsar <Diptanshubhawsar50@gmail.com>
2024-06-02 04:06:54 +0530

    fix minor responsiveness

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import ScrollIcon from "@/components/scrollIcon/ScrollIcon";
 import Canvas from "@/components/canvas";
 
@@ -13,26 +13,24 @@ const Home = () => {
       </div>
 
       <div className="h-screen pt-20 relative z-10">
-        <div className="pt-20 md:px-8 flex flex-col justify-between h-[80%]">
-          <div className="m-8">
-            <div className="flex justify-start flex-col items-start text-xl font-normal md:text-3xl pl-8">
-              <p>Frontend Developer / Designer</p>
+        <div className="flex flex-col justify-between h-[80%] pt-20 md:px-8">
+          <div className="md:m-8">
+            <div className="flex flex-col items-start text-xl font-normal md:text-3xl pl-8 fade-in">
+              <p className="fade-in" style={{ animationDelay: "0.5s" }}>Frontend Expert / Designer</p>
+              <p className="fade-in" style={{ animationDelay: "1s" }}>Full-Stack Developer</p>
             </div>
 
-            <div className="flex justify-start flex-col items-start my-5 text-3xl md:text-6xl font-extrabold pl-8">
-              <p>Beyond the extraordinary:</p>
-              <p>
-                <span className="text-purple-600">Original </span>
-                and <span className="text-purple-600">distinctive</span>{" "}
+            <div className="flex flex-col items-start my-5 text-3xl md:text-6xl font-extrabold pl-8">
+              <p className="fade-in" style={{ animationDelay: "1.5s" }}>Beyond the extraordinary:</p>
+              <p className="fade-in" style={{ animationDelay: "2s" }}>
+                <span className="text-purple-600">Original</span> and <span className="text-purple-600">distinctive</span>
               </p>
-              <p>Design X development</p>
+              <p className="fade-in" style={{ animationDelay: "2.5s" }}>Design X development</p>
             </div>
           </div>
 
-          <div className="p-4 m-8">
-            <span className="font-normal text-xl md:text-2xl mx-3">
-              Diptanshu Bhawsar
-            </span>
+          <div className="m-8 fade-in" style={{ animationDelay: "3s" }}>
+            <span className="font-normal text-xl md:text-2xl">Diptanshu Bhawsar</span>
             <div className="flex justify-center items-center mt-8 md:mt-0">
               <ScrollIcon />
             </div>
